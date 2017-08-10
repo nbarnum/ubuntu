f2ccf58d7d2093823752c527e6de2329d43ae0df
nbarnum <nbarnum@users.noreply.github.com>
2017-08-10 14:38:32 -0600

    adding install of saltstack 2014.7.5

--- custom-script.sh
+++ custom-script.sh
@@ -2,8 +2,11 @@
 
 set -eux
 
-# Sample custom configuration script - add your own commands here
-# to add some additional commands for your environment
-#
-# For example:
-# yum install -y curl wget git tmux firefox xvfb
+apt-get update
+apt-get install -y curl
+# download the old version of the bootstrap script to play nice with the old salt
+curl -s -o /tmp/bootstrap-salt.sh 'https://raw.githubusercontent.com/saltstack/salt-bootstrap/4c2e1bcd56c87493ebd37783db19f88e94e5b444/bootstrap-salt.sh'
+# run the bootstrap script and install the crusty 2014.7.5 from github
+sh /tmp/bootstrap-salt.sh -P git v2014.7.5
+
+rm /tmp/bootstrap-salt.sh
