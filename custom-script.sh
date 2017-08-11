bbc742ea0660ee9f154cf1371c895db1afb6f100
nbarnum <nbarnum@users.noreply.github.com>
2017-08-11 15:33:13 -0600

    fixing issue with minion_id getting set as vagrant

--- custom-script.sh
+++ custom-script.sh
@@ -2,11 +2,18 @@
 
 set -eux
 
-apt-get update
-apt-get install -y curl
+apt-get update -qq
+apt-get install -qqy curl
 # download the old version of the bootstrap script to play nice with the old salt
 curl -s -o /tmp/bootstrap-salt.sh 'https://raw.githubusercontent.com/saltstack/salt-bootstrap/4c2e1bcd56c87493ebd37783db19f88e94e5b444/bootstrap-salt.sh'
 # run the bootstrap script and install the crusty 2014.7.5 from github
 sh /tmp/bootstrap-salt.sh -P git v2014.7.5
-
+# remove the bootstrap script once we're done with it
 rm /tmp/bootstrap-salt.sh
+
+# stop salt-minion service
+stop salt-minion
+# remove the minion_id so that it can be set by the hostname on future calls
+rm /etc/salt/minion_id
+# disable automatic starting of minion (so it doesn't set the minion_id again before hostname is updated)
+echo manual >> /etc/init/salt-minion.override
--- script/motd.sh
+++ script/motd.sh
@@ -8,7 +8,8 @@ MOTD_FILE=/etc/motd
 BANNER_WIDTH=64
 PLATFORM_RELEASE=$(lsb_release -sd)
 PLATFORM_MSG=$(printf '%s' "$PLATFORM_RELEASE")
-BUILT_MSG=$(printf 'built %s' $(date +%Y-%m-%d))
+# BUILT_MSG=$(printf 'built %s' $(date +%Y-%m-%d))
+BUILT_MSG="SaltStack v2014.7.5"
 printf '%0.1s' "-"{1..64} > ${MOTD_FILE}
 printf '\n' >> ${MOTD_FILE}
 printf '%2s%-30s%30s\n' " " "${PLATFORM_MSG}" "${BUILT_MSG}" >> ${MOTD_FILE}
